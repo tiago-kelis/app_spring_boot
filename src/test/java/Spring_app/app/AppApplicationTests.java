3b6ecb41deb311cc855397b277b10df4b0e0fdb3
tiago-kelis <tiagokelis2017@gmail.com>
2024-09-13 20:56:15 -0300

    construct__1

---
+++ src/test/java/Spring_app/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_app.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1e974c71747f9f70795133f3af20d95db6310c85
tiago-kelis <tiagokelis2017@gmail.com>
2024-09-14 22:28:19 -0300

    Update Exceptions

--- src/main/java/Spring_app/app/services/UserService.java
+++ src/main/java/Spring_app/app/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import Spring_app.app.entities.User;
 import Spring_app.app.repositories.UserRepository;
+import Spring_app.app.services.exception.ResourceNotFoundException;
 
 
 @Service 
@@ -25,7 +26,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get(); 
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); 
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/Spring_app/app/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package Spring_app.app.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		
+		super("Resource not found. id " + id);
+		
+	}
+
+}
